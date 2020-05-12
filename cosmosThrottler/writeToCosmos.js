904bf2b4d848b5d60ffedaa58a3295501145dab1
David McGinnis <656337+davidov541@users.noreply.github.com>
2020-05-11 22:55:43 -0500

    We now parse the string before using it.

--- cosmosThrottler/writeToCosmos.js
+++ cosmosThrottler/writeToCosmos.js
@@ -4,16 +4,18 @@ module.exports = async function(context, msg) {
     console.log("Context: " + JSON.stringify(context));
     console.log("Message: " + JSON.stringify(msg));
     console.log("Message2: " + msg);
-    console.log("Entity type: " + msg.entityType);
-    switch(msg.entityType)
+    const parsed = JSON.parse(msg);
+    console.log("Message3: " + parsed);
+    console.log("Entity type: " + parsed.entityType);
+    switch(parsed.entityType)
     {
         case 'vertex':
             console.log("Adding vertex")
-            await cosmos.createEntryOfKind(msg.kind, msg.id, msg.properties, msg.edges);
+            await cosmos.createEntryOfKind(parsed.kind, parsed.id, parsed.properties, parsed.edges);
             break;
         case 'edge':
             console.log("Adding edge")
-            await cosmos.createEdge(msg.source, msg.target, msg.relationship, msg.properties);
+            await cosmos.createEdge(parsed.source, parsed.target, parsed.relationship, parsed.properties);
             break;
     }
 };
