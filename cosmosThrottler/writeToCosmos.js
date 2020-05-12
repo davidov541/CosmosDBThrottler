be12ea3597c9091c41e9ced668f23410f8cf5029
David McGinnis <656337+davidov541@users.noreply.github.com>
2020-05-11 22:52:37 -0500

    Adding some more logging.

--- cosmosThrottler/writeToCosmos.js
+++ cosmosThrottler/writeToCosmos.js
@@ -4,12 +4,15 @@ module.exports = async function(context, msg) {
     console.log("Context: " + JSON.stringify(context));
     console.log("Message: " + JSON.stringify(msg));
     console.log("Message2: " + msg);
+    console.log("Entity type: " + msg.entityType);
     switch(msg.entityType)
     {
         case 'vertex':
+            console.log("Adding vertex")
             await cosmos.createEntryOfKind(msg.kind, msg.id, msg.properties, msg.edges);
             break;
         case 'edge':
+            console.log("Adding edge")
             await cosmos.createEdge(msg.source, msg.target, msg.relationship, msg.properties);
             break;
     }
