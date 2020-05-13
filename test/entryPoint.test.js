a3022803e065b3c6f8b33488ae04504d9e9ef7fa
David McGinnis <656337+davidov541@users.noreply.github.com>
2020-05-12 21:52:45 -0500

    Adding the rest of the tests for the main entrypoint.

--- test/entryPoint.test.js
+++ test/entryPoint.test.js
@@ -1,14 +1,24 @@
 const rewire = require('rewire');
 const sinon = require('sinon');
 
-const uut = rewire('../cosmosThrottler/writeToCosmos'); // the module to test
-const createEntryOfKindSpy = sinon.spy(); // the fake method on the dependency
+const uut = rewire('../cosmosThrottler/writeToCosmos');
+const createEntryOfKindSpy = sinon.spy();
+const createEdgeSpy = sinon.spy();
+const deleteEntrySpy = sinon.spy();
 const cosmosMock = {
-    createEntryOfKind: createEntryOfKindSpy
+    createEntryOfKind: createEntryOfKindSpy,
+    createEdge: createEdgeSpy,
+    deleteEntry: deleteEntrySpy
 };
 uut.__set__("cosmos", cosmosMock);
 
 describe('Entry Point Tests', function () {
+    beforeEach(() => {
+        createEntryOfKindSpy.resetHistory();
+        createEdgeSpy.resetHistory();
+        deleteEntrySpy.resetHistory();
+    });
+
     test('should request cosmos to create the vertex', async function () {
         const message = {
             entityType: "vertex",
@@ -34,5 +44,46 @@ describe('Entry Point Tests', function () {
         expect(args[1]).toBe(message.id);
         expect(args[2]).toEqual(message.properties);
         expect(args[3]).toEqual(message.edges);
+    });
+
+    test('should request cosmos to create the edge', async function () {
+        const message = {
+            entityType: "edge",
+            source: "someSource",
+            target: "someTarget",
+            relationship: "fakeRelationship",
+            properties: {
+                property1: "prop1val",
+                property2: "prop2val"
+            }
+        }
+
+        const context = {};
+        await uut(context, JSON.stringify(message));
+
+        expect(createEdgeSpy.called).toBeTruthy();
+        expect(createEdgeSpy.callCount).toBe(1);
+        const args = createEdgeSpy.args[0];
+        expect(args[0]).toBe(message.source);
+        expect(args[1]).toBe(message.target);
+        expect(args[2]).toBe(message.relationship);
+        expect(args[3]).toEqual(message.properties);
+    });
+
+    test('should request cosmos to delete an entity', async function () {
+        const message = {
+            entityType: "deletion",
+            id: "someEntity",
+            edgeLabelsToFollow: ["label1", "label2"]
+        }
+
+        const context = {};
+        await uut(context, JSON.stringify(message));
+
+        expect(deleteEntrySpy.called).toBeTruthy();
+        expect(deleteEntrySpy.callCount).toBe(1);
+        const args = deleteEntrySpy.args[0];
+        expect(args[0]).toBe(message.id);
+        expect(args[1]).toEqual(message.edgeLabelsToFollow);
     });
 });
