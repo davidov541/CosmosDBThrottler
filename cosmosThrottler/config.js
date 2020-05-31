eb43f11a30a1a252fe700e3f6b86e03ee398f89d
David McGinnis <656337+davidov541@users.noreply.github.com>
2020-05-31 15:16:05 -0500

    Switching to using environment variables to track configuration.

---
+++ cosmosThrottler/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "endpoint": process.env.ApplicationVariables_Endpoint,
+    "primaryKey": process.env.ApplicationVariables_PrimaryKey,
+    "database": process.env.ApplicationVariables_Database,
+    "collection": process.env.ApplicationVariables_Collection,
+    "env": process.env.NODE_ENV
+}
