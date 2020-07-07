fed009ed4122ef470b477cc83d5148d332a1f02f
David McGinnis <656337+davidov541@users.noreply.github.com>
2020-07-07 11:05:03 -0500

    Updating names for entity type to allow for add-edge later.

--- cosmosThrottler/writeToCosmos.js
+++ cosmosThrottler/writeToCosmos.js
@@ -5,13 +5,13 @@ module.exports = async function(_, msg) {
     console.log(cosmos);
     switch(parsed.entityType)
     {
-        case 'vertex':
+        case 'add-vertex':
             await cosmos.createEntryOfKind(parsed.kind, parsed.id, parsed.properties, parsed.edges);
             break;
-        case 'edge':
+        case 'add-edge':
             await cosmos.createEdge(parsed.source, parsed.target, parsed.relationship, parsed.properties);
             break;
-        case 'deletion':
+        case 'delete-vertex':
             await cosmos.deleteEntry(parsed.id, parsed.edgeLabelsToFollow);
             break;
     }
--- test/entryPoint.test.js
+++ test/entryPoint.test.js
@@ -21,7 +21,7 @@ describe('Entry Point Tests', function () {
 
     test('should request cosmos to create the vertex', async function () {
         const message = {
-            entityType: "vertex",
+            entityType: "add-vertex",
             kind: "fakeKind",
             id: "someID",
             properties: {
@@ -48,7 +48,7 @@ describe('Entry Point Tests', function () {
 
     test('should request cosmos to create the edge', async function () {
         const message = {
-            entityType: "edge",
+            entityType: "add-edge",
             source: "someSource",
             target: "someTarget",
             relationship: "fakeRelationship",
@@ -72,7 +72,7 @@ describe('Entry Point Tests', function () {
 
     test('should request cosmos to delete an entity', async function () {
         const message = {
-            entityType: "deletion",
+            entityType: "delete-vertex",
             id: "someEntity",
             edgeLabelsToFollow: ["label1", "label2"]
         }
